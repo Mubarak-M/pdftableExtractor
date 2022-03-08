783e1dd63adb5230e685fa7d73c44a8ec15d343e
Mubarak Mojoyinola <mubarak-mojoyinola@uiowa.edu>
2022-03-08 07:48:04 -0600

    test page error

--- tests/testthat/test_page_error.R
+++ tests/testthat/test_page_error.R
@@ -5,3 +5,25 @@ test_that('page out of range', {
 
 })
 
+library(tidyverse)
+data <- diamonds
+summarise(data, average = mean(price))
+
+summarise(data, mean(price))
+
+arrange(data, desc(price), carat)
+arrange(data, desc(price), desc(carat))
+
+x <- arrange(data, desc(price))
+x[1:20,]
+
+y <- mutate(congress_age,
+       total = n(),
+       prop_democrat = mean(age)
+)
+
+library(fivethirtyeight)
+str(congress_age)
+y
+select(y, bioguide:lastname)
+select(y, lastname:freetime)
